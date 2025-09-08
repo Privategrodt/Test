7ccb768f3c46aeb0582c2a7af2fff54e2666e211
Privategrodt <grodt812@gmail.com>
2025-09-08 17:23:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
 
+export default defineConfig({
+  base: './',
+});
+
   export default defineConfig({
     plugins: [react()],
     resolve: {
